7c8d3e76000cc6a58e8b69e4bc14bd33779c435b
Ágata de Oliveira <agata.rodrigues0927@gmail.com>
2025-07-23 17:09:48 -0300

    Adicao da tela de inicio, mudanca na localizacao das telas de cont de chaves e exclusao da pasta achados e perdidos

--- core/views.py
+++ core/views.py
@@ -4,12 +4,9 @@ def index(request):
     return render (request, 'index.html')
 def materiais(request):
     return render (request, 'materiais.html')
-<<<<<<< HEAD
 def itens(request):
     return render (request, 'itens.html')
 def emprestimos(request):
     return render (request, 'emprestimos.html')
 def estoque(request):
     return render (request, 'estoque.html')
-=======
->>>>>>> 6e76506336cd7eb7aa38ea3995c45e8d9c926b0d

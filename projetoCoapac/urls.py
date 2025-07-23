ccf007523dcd91832cba49e23d77b8f7f0c0a896
Dayane <dayanedulcesantos@gmail.com>
2025-07-23 16:13:42 -0300

    configurei rotar urls e views de admins html

--- projetoCoapac/urls.py
+++ projetoCoapac/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import admins,index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', index, name= 'index'),
+    path('admins/', admins, name='admins'),
 ]

77c6ef6529f6b4f56b30bd91543038c11b47e728
Ágata de Oliveira <agata.rodrigues0927@gmail.com>
2025-10-24 11:37:43 -0300

    Alterações nas urls e views incompletas

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ComunidadeEscolar, Usuarios, TipoProduto, Produtos, Emprestimo
+from .forms import ComunidadeEscolarForm, UsuariosForm, TipoProdutoForm, ProdutosForm, EmprestimoForm
+
 
 # Ordenar alfabeticamente para encontrar mais fácil
 def index(request):
@@ -15,7 +18,34 @@ def itensCadastroNaoDevolutivos(request):
     return render(request, 'itensCadastroNaoDevolutivos.html')
 def itensVisualizacao(request):
     return render(request, 'itensVisualizacao.html')
-def usuariosCadastro(request):
-    return render(request, 'usuarioCadastro.html')
+
+# Usuários
 def usuariosVisualizacao(request):
-    return render(request, 'usuarioVisualizacao.html')
+    usuarios = Usuarios.objects.all()
+    contexto = {
+        'usuarios': usuarios
+    }
+    return render(request, 'usuariosVisualizacao.html', contexto)
+
+def usuariosCadastro(request):
+    form = UsuariosForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('usuariosVisualizacao')
+    
+    contexto = {
+        'form': form
+    }
+    return render(request, 'usuariosCadastro.html', contexto)
+
+def usuarios_editar(request, credencial):
+    usuarios = Usuarios.objects.get(credencial=credencial)
+    form = UsuariosForm(request.POST or None, instance=usuarios)
+    if form.is_valid():
+        form.save()
+        return redirect('usuariosVisualizacao')
+    
+    contexto = {
+        'form': form
+    }
+    return render(request, 'usuariosCadastro.html', contexto)
